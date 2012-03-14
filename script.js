208a8d8b7ca40668cf7b6ce5597bc08b0f2c8843
Filip Wiśniewski <ffwisniewski@gmail.com>
2012-03-14 19:14:40 +0100

    a new approach

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).load(function () {
+	alert("dupa");
+//	$(".div_header").click(function(){
+//		var divId = $(this).attr('id');
+//		$('#' + divId + "_content").toggle('slow');
+//		return false;
+//	});
+});
