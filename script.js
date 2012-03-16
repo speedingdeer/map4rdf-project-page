67eb1eb9d08a11e0444b7f207d00811eb1c98ebe
Filip Wiśniewski <ffwisniewski@gmail.com>
2012-03-16 15:54:34 +0100

    outline

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+var last_color = "#F1F2F5"; //otherwise last colour is the hover colour
+var last_elem = null;
+var last_border_bottom = null;
+
+function openBookmark(selector) {
+	$(selector).each(function(){
+			//manu part
+			if (last_elem!=null) {
+			//	last_elem.css('background-color',last_color);
+			//	last_elem.css('border-bottom',last_border_bottom);
+			last_elem.removeClass('marked-bookmark');
+			}
+		
+			last_elem=$(this);
+			last_elem.addClass('marked-bookmark');
+			//last_border_bottom=$(this).css('border-bottom');
+			
+			//$(this).css('background-color','#FFFFFF');
+			//$(this).css('border-bottom','none');
+			
+			//fillup content part
+			$('#content').html($(selector+"_content").html());
+			
+		});
+	}
+
 $(window).load(function () {
-	alert("dupa");
-//	$(".div_header").click(function(){
-//		var divId = $(this).attr('id');
-//		$('#' + divId + "_content").toggle('slow');
-//		return false;
-//	});
+	$(".bookmark").each(function(){
+		$(this).click(function(){
+			var selector = '#' + $(this).attr('id');
+			openBookmark(selector);
+		})
+	});
+	//site initialization
+	openBookmark("#about");
 });
