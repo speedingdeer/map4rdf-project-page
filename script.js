b79d54712c7ab8978e7ea98df77136aea86b61e2
Filip Wiśniewski <ffwisniewski@gmail.com>
2012-03-20 16:05:46 +0100

    ...

--- script.js
+++ script.js
@@ -6,19 +6,10 @@ function openBookmark(selector) {
 	$(selector).each(function(){
 			//manu part
 			if (last_elem!=null) {
-			//	last_elem.css('background-color',last_color);
-			//	last_elem.css('border-bottom',last_border_bottom);
-			last_elem.removeClass('marked-bookmark');
-			}
-		
+				last_elem.removeClass('marked-bookmark');
+			}	
 			last_elem=$(this);
 			last_elem.addClass('marked-bookmark');
-			//last_border_bottom=$(this).css('border-bottom');
-			
-			//$(this).css('background-color','#FFFFFF');
-			//$(this).css('border-bottom','none');
-			
-			//fillup content part
 			$('#content').html($(selector+"_content").html());
 			
 		});
@@ -32,5 +23,5 @@ $(window).load(function () {
 		})
 	});
 	//site initialization
-	openBookmark("#download");
+	openBookmark("#about");
 });
